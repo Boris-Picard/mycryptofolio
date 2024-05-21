feb6344e9d254b339392d45dfa962261d5ac1abe
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 <picard.boris@gmail.com>
2024-05-21 10:30:01 +0200

    ajout des inputs et validation avec .parse

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import FormTemplate from "./components/FormTemplate"
 function App() {
 
   return (
-    <div className="container">
+    <div className="container h-screen">
       <FormTemplate />
     </div>
   )
--- src/components/FormTemplate.jsx
+++ src/components/FormTemplate.jsx
@@ -20,41 +20,94 @@ export default function FormTemplate() {
         username: z.string().min(2, {
             message: "Username must be at least 2 characters.",
         }),
+        lastname: z.string().min(2, {
+            message: "lastname must be at least 2 characters.",
+        }),
+        password: z.string().refine(value => value.length >= 8, {
+            message: "Password must be at least 8 characters long",
+        }),
     })
 
     const form = useForm({
         resolver: zodResolver(FormSchema),
         defaultValues: {
             username: "",
+            lastname: "",
+            password: "",
         }
     })
 
     const onSubmit = (data) => {
-        console.log(data);
+        try {
+            FormSchema.parse(data);
+            console.log("Form data is valid");
+            console.log(data);
+        } catch (error) {
+            console.log("Form data is invalid", error.message);
+        }
     }
 
     return (
-        <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6">
-                <FormField
-                    control={form.control}
-                    name="username"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Username</FormLabel>
-                            <FormControl>
-                                <Input placeholder="shadcn" {...field} />
-                            </FormControl>
-                            <FormDescription>
-                                This is your public display name.
-                            </FormDescription>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-                <Button type="submit">Submit</Button>
-            </form>
-        </Form >
+        <div className="flex min-h-screen items-center justify-center">
+            <div className="flex w-1/2 flex-col justify-center items-center shadow-lg p-6 rounded-md bg-slate-200">
+                <div className="flex flex-col space-y-3 items-center">
+                    <h1 className="font-bold text-5xl">
+                        Welcome
+                    </h1>
+                    <h2 className="font-bold text-3xl">
+                        Sign in
+                    </h2>
+                </div>
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4 w-full">
+                        <div className="grid grid-cols-2 gap-6 my-3">
+                            <FormField
+                                control={form.control}
+                                name="username"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Username</FormLabel>
+                                        <FormControl>
+                                            <Input placeholder="Boris" {...field} />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={form.control}
+                                name="lastname"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>lastname</FormLabel>
+                                        <FormControl>
+                                            <Input placeholder="Picard" {...field} />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <div className="col-span-2">
+                                <FormField
+                                    control={form.control}
+                                    name="password"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormLabel>password</FormLabel>
+                                            <FormControl>
+                                                <Input placeholder="Picard" {...field} />
+                                            </FormControl>
+                                            <FormMessage />
+                                        </FormItem>
+                                    )}
+                                />
+                            </div>
+                        </div>
+                        <Button type="submit">Submit</Button>
+                    </form>
+                </Form >
+            </div>
+        </div>
     )
 }
 
