bbebedbe1928ecfe57cabf9959049559bc00c967
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 <picard.boris@gmail.com>
2024-05-24 12:14:41 +0200

    multip step form in progress

--- src/components/FormTemplate.jsx
+++ src/components/FormTemplate.jsx
@@ -17,30 +17,21 @@ import { Input } from "@/components/ui/input"
 export default function FormTemplate() {
 
     const FormSchema = z.object({
-        username: z.string().min(2, {
-            message: "Username must be at least 2 characters.",
-        }),
-        lastname: z.string().min(2, {
-            message: "lastname must be at least 2 characters.",
-        }),
-        password: z.string().refine(value => value.length >= 8, {
-            message: "Password must be at least 8 characters long",
+        coin: z.string().min(2, {
+            message: "Coin must be at least 2 characters.",
         }),
     })
 
     const form = useForm({
         resolver: zodResolver(FormSchema),
         defaultValues: {
-            username: "",
-            lastname: "",
-            password: "",
+            coin: "",
         }
     })
 
     const onSubmit = (data) => {
         try {
             FormSchema.parse(data);
-            console.log("Form data is valid");
             console.log(data);
         } catch (error) {
             console.log("Form data is invalid", error.message);
@@ -55,55 +46,25 @@ export default function FormTemplate() {
                         Welcome
                     </h1>
                     <h2 className="font-bold text-3xl">
-                        Sign in
+                        Add a coin
                     </h2>
                 </div>
                 <Form {...form}>
                     <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4 w-full">
-                        <div className="grid grid-cols-2 gap-6 my-3">
-                            <FormField
-                                control={form.control}
-                                name="username"
-                                render={({ field }) => (
-                                    <FormItem>
-                                        <FormLabel>Username</FormLabel>
-                                        <FormControl>
-                                            <Input placeholder="Boris" {...field} />
-                                        </FormControl>
-                                        <FormMessage />
-                                    </FormItem>
-                                )}
-                            />
-                            <FormField
-                                control={form.control}
-                                name="lastname"
-                                render={({ field }) => (
-                                    <FormItem>
-                                        <FormLabel>lastname</FormLabel>
-                                        <FormControl>
-                                            <Input placeholder="Picard" {...field} />
-                                        </FormControl>
-                                        <FormMessage />
-                                    </FormItem>
-                                )}
-                            />
-                            <div className="col-span-2">
-                                <FormField
-                                    control={form.control}
-                                    name="password"
-                                    render={({ field }) => (
-                                        <FormItem>
-                                            <FormLabel>password</FormLabel>
-                                            <FormControl>
-                                                <Input placeholder="Picard" {...field} />
-                                            </FormControl>
-                                            <FormMessage />
-                                        </FormItem>
-                                    )}
-                                />
-                            </div>
-                        </div>
-                        <Button type="submit">Submit</Button>
+                        <FormField
+                            control={form.control}
+                            name="coin"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Coin</FormLabel>
+                                    <FormControl>
+                                        <Input placeholder="Bitcoin" {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <Button type="submit">Next Step</Button>
                     </form>
                 </Form >
             </div>
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import path from "path"
-import react from "@vitejs/plugin-react-swc"
-import { defineConfig } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
@@ -9,4 +9,15 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  // connection entre le front (vite) et le back (express)
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3001",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
