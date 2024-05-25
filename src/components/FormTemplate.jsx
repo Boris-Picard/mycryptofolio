38878a5f2b1ea722bb9c7a22f89f4b3fa6909012
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 <picard.boris@gmail.com>
2024-05-25 15:37:03 +0200

    ajout date picker shadcn

--- src/components/FormTemplate.jsx
+++ src/components/FormTemplate.jsx
@@ -23,11 +23,21 @@ import {
 } from "@/components/ui/select"
 import { useState } from "react"
 
+import { format } from "date-fns"
+import { CalendarIcon } from "lucide-react"
+import { Calendar } from "@/components/ui/calendar"
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/components/ui/popover"
+import { cn } from "@/lib/utils"
+
 export default function FormTemplate() {
 
     const [steps, setSteps] = useState(1)
-
     const [firstStep, setFirstStep] = useState([])
+
 
     const list = listData;
     const parsedlist = JSON.parse(JSON.stringify(list));
@@ -62,7 +72,7 @@ export default function FormTemplate() {
             quantity: 0,
             price: 0,
             spent: 0,
-            date: ""
+            date: new Date()
         }
     })
 
@@ -194,11 +204,39 @@ export default function FormTemplate() {
                                         control={form.control}
                                         name="date"
                                         render={({ field }) => (
-                                            <FormItem>
-                                                <FormLabel>Date</FormLabel>
-                                                <FormControl>
-                                                    <Input type="date" {...field} />
-                                                </FormControl>
+                                            <FormItem className="flex flex-col">
+                                                <FormLabel>Date of birth</FormLabel>
+                                                <Popover>
+                                                    <PopoverTrigger asChild>
+                                                        <FormControl>
+                                                            <Button
+                                                                variant={"outline"}
+                                                                className={cn(
+                                                                    "w-[240px] pl-3 text-left font-normal",
+                                                                    !field.value && "text-muted-foreground"
+                                                                )}
+                                                            >
+                                                                {field.value ? (
+                                                                    format(field.value, "PPP")
+                                                                ) : (
+                                                                    <span>Pick a date</span>
+                                                                )}
+                                                                <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                                                            </Button>
+                                                        </FormControl>
+                                                    </PopoverTrigger>
+                                                    <PopoverContent className="w-auto p-0" align="start">
+                                                        <Calendar
+                                                            mode="single"
+                                                            selected={field.value}
+                                                            onSelect={field.onChange}
+                                                            disabled={(date) =>
+                                                                date > new Date() || date < new Date("1900-01-01")
+                                                            }
+                                                            initialFocus
+                                                        />
+                                                    </PopoverContent>
+                                                </Popover>
                                                 <FormMessage />
                                             </FormItem>
                                         )}
