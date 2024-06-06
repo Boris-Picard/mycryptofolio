4862f6c89e89b04cad514578753dbdb56c0fadb2
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 <picard.boris@gmail.com>
2024-06-06 17:23:13 +0200

    ajout total des profits et pertes

--- src/components/CardsData.jsx
+++ src/components/CardsData.jsx
@@ -12,14 +12,21 @@ import { useEffect, useState } from "react"
 export default function CardsData({ transactions }) {
     console.log(transactions);
     const [totalInvested, setTotalInvested] = useState(0);
+    const [totalGain, setTotalGain] = useState(0)
+
     useEffect(() => {
         const totalAmount = transactions.reduce((acc, transaction) => acc + transaction.actualValue, 0)
+        const totalWinOrLoss = transactions.reduce((acc, transaction) => acc + transaction.actualPrice, 0)
+        // const totalWinOrLossPercent = 
+        // console.log(totalWinOrLossPercent);
         setTotalInvested(totalAmount)
+        setTotalGain(totalWinOrLoss)
     }, [transactions])
+
     return (<>
         <Card>
             <CardHeader>
-                <CardTitle>{totalInvested} $US</CardTitle>
+                <CardTitle>{totalInvested.toFixed(2)} $US</CardTitle>
                 <CardDescription>Solde actuel</CardDescription>
             </CardHeader>
         </Card>
@@ -31,8 +38,8 @@ export default function CardsData({ transactions }) {
         </Card>
         <Card>
             <CardHeader>
-                <CardTitle>Card Title</CardTitle>
-                <CardDescription>Solde actuel</CardDescription>
+                <CardTitle>{totalGain.toFixed(2)} $US</CardTitle>
+                <CardDescription>Total des profits et pertes</CardDescription>
             </CardHeader>
         </Card>
         <Card>
