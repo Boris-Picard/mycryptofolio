9a0d76dbe75922640f9a21edfad86c3151e94d82
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 <picard.boris@gmail.com>
2024-06-07 11:41:10 +0200

    ajout bestwinner

--- src/components/CardsData.jsx
+++ src/components/CardsData.jsx
@@ -13,14 +13,21 @@ export default function CardsData({ transactions }) {
     console.log(transactions);
     const [totalInvested, setTotalInvested] = useState(0);
     const [totalGain, setTotalGain] = useState(0)
+    const [maxTransaction, setMaxTransaction] = useState(0)
+    const [bestWinnerValue, setBestWinnerValue] = useState(0)
 
     useEffect(() => {
         const totalAmount = transactions.reduce((acc, transaction) => acc + transaction.actualValue, 0)
         const totalWinOrLoss = transactions.reduce((acc, transaction) => acc + transaction.actualPrice, 0)
-        // const totalWinOrLossPercent = 
-        // console.log(totalWinOrLossPercent);
+        const percentageWinOrLoss = transactions.reduce((max, transaction) => transaction.gainOrLossPercentage > max.gainOrLossPercentage ? transaction : max, transactions[0])
+        console.log(percentageWinOrLoss);
+        if (percentageWinOrLoss) {
+            const bestWinner = percentageWinOrLoss.actualValue - percentageWinOrLoss.spent
+            setBestWinnerValue(bestWinner)
+        }
         setTotalInvested(totalAmount)
         setTotalGain(totalWinOrLoss)
+        setMaxTransaction(percentageWinOrLoss)
     }, [transactions])
 
     return (<>
@@ -44,8 +51,8 @@ export default function CardsData({ transactions }) {
         </Card>
         <Card>
             <CardHeader>
-                <CardTitle>Card Title</CardTitle>
-                <CardDescription>Solde actuel</CardDescription>
+                <CardTitle className="flex gap-2"><img src={maxTransaction?.image} alt={maxTransaction?.name} width={24} height={24} />{maxTransaction?.name} <span className="text-slate-500 uppercase font-normal">{maxTransaction?.symbol}</span></CardTitle>
+                <CardDescription>Meilleur Gagnant {bestWinnerValue.toFixed(2)} $US</CardDescription>
             </CardHeader>
         </Card>
     </>
