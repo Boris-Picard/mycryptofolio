373cdfc076e0abb8eb130a7f2a68bfc360080a3f
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 <picard.boris@gmail.com>
2024-06-10 17:03:28 +0200

    ajout component tabledetailed

--- src/Pages/Detailed.jsx
+++ src/Pages/Detailed.jsx
@@ -1,9 +1,37 @@
-import { useLocation } from "react-router-dom";
+import TableDetailed from "@/components/TableDetailed"
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table"
 
 export default function Detailed() {
-    const location = useLocation()
-    const { coin } = location.state // Récupère les données du coin passées via navigate
 
-    console.log(coin);
-    return (<div></div>)
+    return (<div className="container h-screen p-10">
+        <div className="grid grid-cols-4 mb-3 gap-3">
+            {/* <CardsData transactions={transactions} /> */}
+        </div>
+        <Table className="bg-white rounded-xl">
+            <TableCaption>A list of your recent trade</TableCaption>
+            <TableHeader>
+                <TableRow>
+                    <TableHead>#</TableHead>
+                    <TableHead>Monnaie</TableHead>
+                    <TableHead>Cours</TableHead>
+                    <TableHead>24h</TableHead>
+                    <TableHead>Capitalisation boursière</TableHead>
+                    <TableHead>Ath</TableHead>
+                    <TableHead>Participations</TableHead>
+                    <TableHead>Pertes et profits</TableHead>
+                    <TableHead>Actions</TableHead>
+                </TableRow>
+            </TableHeader>
+            <TableBody>
+                <TableDetailed />
+            </TableBody>
+        </Table>
+    </div>)
 }
---
+++ src/components/TableDetailed.jsx
@@ -0,0 +1,33 @@
+import {
+    TableCell,
+    TableRow,
+} from "@/components/ui/table"
+import { useState } from "react";
+
+import { useLocation } from "react-router-dom";
+
+export default function TableDetailed() {
+    const location = useLocation()
+    const { coin } = location.state // Récupère les données du coin passées via navigate
+    const [data, setData] = useState([coin])
+
+    return (
+        <>
+            {data.map((coin, i) => {
+                return <TableRow key={i} className="font-semibold">
+                    <TableCell className="font-medium">{coin.rank}</TableCell>
+                    <TableCell><div className="flex gap-2 items-center"><img src={coin.image} alt={coin.name} width={24} height={24} />{coin.name}</div></TableCell>
+                    <TableCell>{coin.price} $US</TableCell>
+                    <TableCell></TableCell>
+                    <TableCell>{coin.market_cap} $US</TableCell>
+                    <TableCell>{coin.ath} $US</TableCell>
+                    <TableCell>
+                        <div className="flex flex-col">
+                            {coin.actualValue?.toFixed(2)} $US<span className="uppercase text-slate-500 font-normal">{coin.quantity} {coin.symbol}</span>
+                        </div>
+                    </TableCell>
+                </TableRow>
+            })}
+        </>
+    )
+}
