b1759bcebf8f04ba527e2991781b3020b0c3e8a4
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 <picard.boris@gmail.com>
2024-06-12 11:30:47 +0200

    detailed page almost done todo actions

--- src/Pages/Detailed.jsx
+++ src/Pages/Detailed.jsx
@@ -8,12 +8,45 @@ import {
     TableRow,
 } from "@/components/ui/table"
 
+import { useLocation } from "react-router-dom"
 
 import CardsDetailed from "@/components/CardsDetailed"
+import { useEffect, useState } from "react"
 
 export default function Detailed() {
+    const location = useLocation()
+    const { coinData } = location.state
+    console.log(coinData);
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        coinData.map(value => setData({ image: value.image, name: value.coin.name, actual_price: value.price, price_change_24h: value.price_change_24h, symbol: value.symbol }))
+    }, [coinData])
+
+    const arrowUpOrDown = (value) => {
+        if (!value) {
+            return
+        }
+        const direction = value.toString().startsWith("-") ? "down" : "up";
+        return (
+            <div className={`flex font-semibold items-center ${direction === "down" ? "text-red-500" : "text-green-500"}`}>
+                <svg fill="currentColor" className="w-8 h-8" viewBox="0 0 24 24">
+                    <path
+                        d={direction === "up" ? "M7 14l5-5 5 5H7z" : " M7 10l5 5 5-5H7z"}
+                    />
+                </svg>
+                <span>{value.toFixed(1)} %</span>
+            </div>
+        );
+    };
 
     return (<div className="container h-screen p-10">
+        <div className="flex mb-3 gap-3 items-center">
+            <img src={data.image} alt={data.name} width={24} height={24} /><h1 className="text-white capitalize text-2xl font-semibold">{data.name}</h1><span className="text-slate-500 font-semibold uppercase self-end">{data.symbol}</span>
+        </div>
+        <div className="flex mb-5 ">
+            <h2 className="text-white text-3xl font-semibold">{data.actual_price} $US</h2>{arrowUpOrDown(data.price_change_24h)}
+        </div>
         <div className="grid grid-cols-5 mb-3 gap-3">
             <CardsDetailed />
         </div>
