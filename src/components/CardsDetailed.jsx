4bce0101138a1a7cb615aebf4ec590cfde17f00b
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 <picard.boris@gmail.com>
2024-06-12 10:48:09 +0200

    cardDetailed done

--- src/components/CardsDetailed.jsx
+++ src/components/CardsDetailed.jsx
@@ -4,48 +4,74 @@ import {
     CardHeader,
     CardTitle,
 } from "@/components/ui/card"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 import { useLocation } from "react-router-dom"
 
 export default function CardsDetailed() {
     const location = useLocation()
     const { coinData } = location.state
-    console.log(coinData)
+    
+    const [totalValue, setTotalValue] = useState(0)
+    const [totalCoins, setTotalCoins] = useState(0)
+    const [totalSpent, setTotalSpent] = useState(0)
+    const [totalGainOrLoss, setTotalGainOrLoss] = useState(0)
+    const [averageBuyPrice, setAverageBuyPrice] = useState(0)
+    const [symbol, setSymbol] = useState([])
 
     useEffect(() => {
-        const allValue = coinData.reduce((acc, value) => value.actualValue, 0)
+        coinData.map(value => setSymbol(value.symbol))
+        const allValue = coinData.reduce((acc, value) => acc + value.actualValue, 0)
+        const allCoins = coinData.reduce((acc, value) => acc + value.quantity, 0)
+        const spent = coinData.reduce((acc, value) => acc + value.spent, 0)
+        const gainOrLoss = coinData.reduce((acc, value) => acc + value.actualPrice, 0)
+        const averagePrice = spent / allCoins
+        setTotalValue(allValue)
+        setTotalCoins(allCoins)
+        setTotalSpent(spent)
+        setTotalGainOrLoss(gainOrLoss)
+        setAverageBuyPrice(averagePrice)
     }, [coinData])
+
+    const UpOrDown = (value) => {
+        if (!value) {
+            return
+        }
+        const direction = value.toString().startsWith("-") ? "down" : "up";
+        return (
+            <span className={`font-semibold ${direction === "down" ? "text-red-500" : "text-green-500"}`}>{value.toFixed(2)} $US</span>
+        );
+    };
 
     return (<>
         <Card>
             <CardHeader>
-                <CardTitle></CardTitle>
+                <CardTitle>{totalValue?.toFixed(2)} $US</CardTitle>
                 <CardDescription>Valeur des participations</CardDescription>
             </CardHeader>
         </Card>
         <Card>
             <CardHeader>
-                <CardTitle>Card Title</CardTitle>
-                <CardDescription>Solde actuel</CardDescription>
+                <CardTitle>{totalCoins} <span className="uppercase">{symbol}</span></CardTitle>
+                <CardDescription>Participations</CardDescription>
             </CardHeader>
         </Card>
         <Card>
             <CardHeader>
-                <CardTitle></CardTitle>
-                <CardDescription></CardDescription>
+                <CardTitle>{totalSpent} $US</CardTitle>
+                <CardDescription>Coût total</CardDescription>
             </CardHeader>
         </Card>
         <Card>
             <CardHeader>
-                <CardTitle></CardTitle>
-                <CardDescription>Meilleur Gagnant </CardDescription>
+                <CardTitle>{averageBuyPrice?.toFixed(2)} $US</CardTitle>
+                <CardDescription>Coût net moyen</CardDescription>
             </CardHeader>
         </Card>
         <Card>
             <CardHeader>
-                <CardTitle></CardTitle>
-                <CardDescription>Meilleur Gagnant </CardDescription>
+                <CardTitle>{UpOrDown(totalGainOrLoss)}</CardTitle>
+                <CardDescription>Total des profits et pertes</CardDescription>
             </CardHeader>
         </Card>
     </>)
