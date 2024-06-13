43b9c4c3bbf3385c7208314c4edfdf80df90f58e
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 <picard.boris@gmail.com>
2024-06-13 11:33:14 +0200

    correction loading screen

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,13 @@ import HomePage from './Pages/HomePage';
 import SeeCoins from "./Pages/SeeCoins"
 import Detailed from "./Pages/Detailed"
 import Loading from './components/Loading';
-import { useEffect, useState } from 'react';
 
 function App() {
-  const [isLoading, setIsLoading] = useState(true)
 
-  useEffect(() => {
-    setTimeout(() => {
-      setIsLoading(false)
-    }, 2000)
-  }, [])
 
   return (
-    <>
-      {isLoading ? (<div className='flex items-center h-full w-full'><Loading /></div>) : <BrowserRouter><div className="h-screen bg-black">
+    <Loading>
+      <BrowserRouter><div className="h-screen bg-black">
         <Navbar />
         <Routes>
           <Route path="/" element={<HomePage />} />
@@ -26,8 +19,8 @@ function App() {
           <Route path="/detailed/:id" element={<Detailed />} />
           <Route path="/seecoins" element={<SeeCoins />} />
         </Routes>
-      </div></BrowserRouter>}
-    </>
+      </div></BrowserRouter>
+    </Loading>
   );
 }
 
