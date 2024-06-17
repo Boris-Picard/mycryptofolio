2a608a4f66714ed56e15d82ae51110d88e43abbe
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 <picard.boris@gmail.com>
2024-06-17 16:38:18 +0200

    feat(password-input): add components password-input, chore(SignIn): integrate zod correcly with input & regex for password

--- src/Pages/SignIn.jsx
+++ src/Pages/SignIn.jsx
@@ -14,21 +14,36 @@ import {
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { useToast } from "@/components/ui/use-toast"
+import { PasswordInput } from "@/components/ui/password-input"
+
+import { Info } from "lucide-react"
 
 export default function SignIn() {
 
     const { toast } = useToast()
 
+    const passwordValidation = new RegExp(
+        /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,50}$/
+    );
+
     const FormSchema = z.object({
-        username: z.string().min(2, {
-            message: "Username must be at least 2 characters.",
-        }),
+        email: z.string()
+            .min(2, {
+                message: "Email must be at least 2 characters.",
+            })
+            .email({ message: "Email is Invalid" }),
+        password: z.string()
+            .min(8, { message: "min 8" })
+            .max(50, { message: "max 50" })
+            .regex(passwordValidation, {
+                message: 'Your password is not valid',
+            })
     })
 
     const form = useForm({
         resolver: zodResolver(FormSchema),
         defaultValues: {
-            username: "",
+            email: "",
         },
     })
 
@@ -41,28 +56,47 @@ export default function SignIn() {
     }
 
     return (
-        <div>
-            <Form {...form}>
-                <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6">
-                    <FormField
-                        control={form.control}
-                        name="username"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>Username</FormLabel>
-                                <FormControl>
-                                    <Input placeholder="" {...field} />
-                                </FormControl>
-                                <FormDescription>
-                                    This is your public display name.
-                                </FormDescription>
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
-                    <Button type="submit">Submit</Button>
-                </form>
-            </Form>
+        <div className="container">
+            <div className="h-screen flex items-center justify-center">
+                <div className="bg-white rounded-xl p-10 w-2/4">
+                    <Form {...form}>
+                        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-3">
+                            <FormField
+                                control={form.control}
+                                name="email"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Email</FormLabel>
+                                        <FormControl>
+                                            <Input placeholder="example@gmail.com" {...field} />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={form.control}
+                                name="password"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Password</FormLabel>
+                                        <FormControl>
+                                            <PasswordInput {...field} />
+                                        </FormControl>
+                                        <FormMessage />
+                                        <FormDescription className="py-3">
+                                            <div className="flex gap-3">
+                                                <div className="text-red-500"><Info /></div> Password must be between 8 and 50 characters and must contain one uppercase letter, one lowercase letter, one number, and one special character.
+                                            </div>
+                                        </FormDescription>
+                                    </FormItem>
+                                )}
+                            />
+                            <Button type="submit">Submit</Button>
+                        </form>
+                    </Form>
+                </div>
+            </div>
         </div>
     )
 
