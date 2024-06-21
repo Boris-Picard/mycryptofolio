72510b93dd939e2bbbfc029d14ad7c49a93232a2
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 <picard.boris@gmail.com>
2024-06-21 11:57:33 +0200

    refactor(signup): redesign signup page done

--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -27,6 +27,8 @@ import {
 } from "@/components/ui/tooltip"
 import { Link } from "react-router-dom"
 
+import bglogin from "../../assets/bglogin.jpg"
+
 
 export default function SignUp() {
 
@@ -67,62 +69,83 @@ export default function SignUp() {
     }
 
     return (
-        <div className="container">
-            <div className="h-screen flex items-center justify-center">
-                <div className="bg-white rounded-xl p-10 w-2/4">
-                    <div className="flex flex-col justify-center items-center py-6">
-                        <h1 className="font-bold text-6xl">SignUp</h1>
-                        <img src={mycryptofolio} alt="" width={190} height={150} className="brightness-0 invert-0 object-cover" />
+        <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px] bg-white h-screen">
+            <div className="flex items-center justify-center py-12 h-screen">
+                <div className="mx-auto grid w-[350px] gap-6">
+                    <div className="grid gap-2 text-center">
+                        <h1 className="text-3xl font-bold">Sign Up</h1>
+                        <p className="text-balance text-muted-foreground">
+                            Enter your email below to create your account
+                        </p>
                     </div>
                     <Form {...form}>
                         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-3">
-                            <FormField
-                                control={form.control}
-                                name="email"
-                                render={({ field }) => (
-                                    <FormItem>
-                                        <FormLabel>Email</FormLabel>
-                                        <FormControl>
-                                            <Input type="email" autoComplete="email" placeholder="example@gmail.com" {...field} />
-                                        </FormControl>
-                                        <FormMessage />
-                                    </FormItem>
-                                )}
-                            />
-                            <FormField
-                                control={form.control}
-                                name="password"
-                                render={({ field }) => (
-                                    <FormItem>
-                                        <div className="flex gap-2 items-center">
-                                            <FormLabel>Password</FormLabel>
-                                            <TooltipProvider>
-                                                <Tooltip delayDuration={0}>
-                                                    <TooltipTrigger asChild>
-                                                        <Info width={20} height={20} />
-                                                    </TooltipTrigger>
-                                                    <TooltipContent>
-                                                        <p>Password must be between 8 and 50 characters and must contain one uppercase letter, one lowercase letter, one number, and one special character.</p>
-                                                    </TooltipContent>
-                                                </Tooltip>
-                                            </TooltipProvider>
-                                        </div>
-                                        <FormControl>
-                                            <PasswordInput autoComplete="new-password" {...field} />
-                                        </FormControl>
-                                        <FormMessage />
-                                    </FormItem>
-                                )}
-                            />
-                            <div className="flex pt-3">
-                                <Button className="w-full" type="submit">Sign Up</Button>
+                            <div className="grid gap-4">
+                                <div className="grid gap-2">
+                                    <FormField
+                                        control={form.control}
+                                        name="email"
+                                        render={({ field }) => (
+                                            <FormItem>
+                                                <FormLabel>Email</FormLabel>
+                                                <FormControl>
+                                                    <Input type="email" autoComplete="email" placeholder="example@gmail.com" {...field} />
+                                                </FormControl>
+                                                <FormMessage />
+                                            </FormItem>
+                                        )}
+                                    />
+                                </div>
+                                <div className="grid gap-2">
+                                    <FormField
+                                        control={form.control}
+                                        name="password"
+                                        render={({ field }) => (
+                                            <FormItem>
+                                                <div className="flex gap-2 items-center">
+                                                    <FormLabel>Password</FormLabel>
+                                                    <TooltipProvider>
+                                                        <Tooltip delayDuration={0}>
+                                                            <TooltipTrigger asChild>
+                                                                <Info width={20} height={20} />
+                                                            </TooltipTrigger>
+                                                            <TooltipContent>
+                                                                <p>Password must be between 8 and 50 characters and must contain one uppercase letter, one lowercase letter, one number, and one special character.</p>
+                                                            </TooltipContent>
+                                                        </Tooltip>
+                                                    </TooltipProvider>
+                                                </div>
+                                                <FormControl>
+                                                    <PasswordInput autoComplete="new-password" {...field} />
+                                                </FormControl>
+                                                <FormMessage />
+                                            </FormItem>
+                                        )}
+                                    />
+                                </div>
+                                <Button type="submit" className="w-full">
+                                    Sign up
+                                </Button>
+                                <Button variant="outline" className="w-full">
+                                    Sign up with Google
+                                </Button>
                             </div>
                         </form>
                     </Form>
-                    <div className="flex gap-3 pt-3">
-                        <span className="font-normal text-slate-500">Already have an account ?</span><Link className="font-semibold" to="/signin">Sign In</Link>
+                    <div className="mt-4 text-center text-sm">
+                        Already have an account?{" "}
+                        <Link to="/signin" className="underline">
+                            Sign in
+                        </Link>
                     </div>
                 </div>
+            </div>
+            <div className="hidden bg-muted lg:block">
+                <img
+                    src={bglogin}
+                    alt="cryptocurrencybg"
+                    className="h-full w-full object-cover brightness-[0.4] grayscale"
+                />
             </div>
         </div>
     )
