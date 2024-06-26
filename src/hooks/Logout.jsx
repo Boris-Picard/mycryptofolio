ae516819532e25fd691ef9dcb73b8dacfafcd3df
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 <picard.boris@gmail.com>
2024-06-26 19:27:37 +0200

    feat: add logout hook to delete cookie & user in zustand

--- src/hooks/Logout.jsx
+++ src/hooks/Logout.jsx
@@ -1,11 +1,24 @@
 import { Button } from "@/components/ui/button";
 import { useAuthStore } from "@/stores/useAuthStore";
+import axios from "axios";
 
 export const Logout = () => {
   const { clearUser } = useAuthStore();
 
-  const handleLogout = () => {
-    clearUser();
+  const handleLogout = async () => {
+    try {
+      const response = await axios.delete('http://localhost:3001/api/auth/logout', {
+        withCredentials: true,
+      })
+      if (response.status === 200) {
+        clearUser();
+      }
+      if (response.status !== 200) {
+        console.error('logout failed');
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return <Button onClick={handleLogout}>Se déconnecter</Button>;
