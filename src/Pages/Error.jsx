a4f218a3634ccb57856806975d545677b4d8c8bc
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 <picard.boris@gmail.com>
2024-07-01 11:43:31 +0200

    feat: error page desgin done

--- src/Pages/Error.jsx
+++ src/Pages/Error.jsx
@@ -1,13 +1,13 @@
 import mycryptofolio from "../../assets/mycryptofolio.png"
-import { Button } from "@/components/ui/button"
+import { Link } from "react-router-dom"
 
 export default function ErrorPage() {
 
     return <div className="container">
-        <div className="flex justify-center items-center h-full flex-col space-y-3">
-            <img src={mycryptofolio} alt="logo website myscryptofolio" className="invert-0 brightness-0" width={200} height={200} />
-            <span className="font-semibold text-2xl">Oups cette page n'existe pas</span>
-            <Button>Revenir a l'accueil</Button>
+        <div className="flex justify-center items-center h-screen flex-col space-y-3">
+            <img src={mycryptofolio} alt="logo website myscryptofolio" className="brightness-0 dark:brightness-100" width={200} height={200} />
+            <span className="font-semibold text-2xl dark:text-white">Oups cette page n'existe pas</span>
+            <Link className="dark:bg-white bg-black text-white dark:text-black py-2 px-3 rounded-md font-semibold hover:bg-slate-900/90 dark:bg:slate-50/90" to="/">Revenir a l'accueil</Link>
         </div>
     </div>
 }
